57833a68801ff7d0e3497f928cb06be6cefeb46a
Derrick Jamal <jamalderrick6@gmail.com>
2023-01-19 20:22:40 +0300

    Fix: Edit title

--- layouts/Main.tsx
+++ layouts/Main.tsx
@@ -7,7 +7,7 @@ type LayoutType = {
   children?: React.ReactNode;
 }
 
-export default ({ children, title = 'Next.js Ecommerce' }: LayoutType) => {
+export default ({ children, title = 'Agricommerce' }: LayoutType) => {
   const router = useRouter();
   const pathname = router.pathname;
 
